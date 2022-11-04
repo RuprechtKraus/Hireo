2e8dce3e86d17d1b141f416e3636918ed68ecbe9
Ruprecht <43097128+RuprechtKraus@users.noreply.github.com>
2022-11-04 19:08:55 +0300

    Добавлена страница регистрации (#4)

--- Hireo.Site/ClientApp/src/app/features/authorization/authorization.module.ts
+++ Hireo.Site/ClientApp/src/app/features/authorization/authorization.module.ts
@@ -3,13 +3,10 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { AuthorizationRoutingModule } from './authorization-routing.module';
+import { RegistrationComponent } from './registration/registration.component';
 
 @NgModule({
-  declarations: [LoginComponent],
+  declarations: [LoginComponent, RegistrationComponent],
   imports: [CommonModule, AuthorizationRoutingModule, SharedModule],
 })
-export class AuthorizationModule {
-  constructor() {
-    console.log("AuthorizationModule");
-  }
-}
+export class AuthorizationModule {}
