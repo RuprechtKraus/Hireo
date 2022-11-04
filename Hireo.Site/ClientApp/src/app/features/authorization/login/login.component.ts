f97d6f45a7c2acb2a5ee6289d821132af423239c
Ruprecht <43097128+RuprechtKraus@users.noreply.github.com>
2022-11-04 21:45:44 +0300

    Добавлена валидация полей логина и регистрации (#6)

--- Hireo.Site/ClientApp/src/app/features/authorization/login/login.component.ts
+++ Hireo.Site/ClientApp/src/app/features/authorization/login/login.component.ts
@@ -4,23 +4,39 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
   loginForm: FormGroup = this._formBuilder.group({
-    email: ["", Validators.required],
-    password: ["", Validators.required]
+    email: ['', [Validators.required, Validators.email]],
+    password: [
+      '',
+      [Validators.required, Validators.minLength(8), Validators.maxLength(32)],
+    ],
   });
 
-  constructor(private _formBuilder: FormBuilder) { }
+  submitted: boolean = false;
 
-  ngOnInit(): void {
+  public get FormControls() {
+    return this.loginForm.controls;
   }
+
+  constructor(private _formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {}
 
   onSubmit(): void {
+    this.submitted = true;
+
     if (this.loginForm.invalid) {
-      console.log("INVALID");
+      console.log('FAILURE');
+      return;
     }
+
+    console.log('SUCCESS');
   }
-  
+
+  isControlValid(controlName: string): boolean {
+    return !this.FormControls[controlName].errors;
+  }
 }
